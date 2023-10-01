0ba6de9c9b7584863d223446cb71ccb292322231
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-02 00:39:35 +0400

    add data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const token = "6537000218:AAGorXhX46H45K1kpt1W1MpdnCBuZwGwero";
 
 const bot = new TelegramBot(token, { polling: true });
-const webAppUrl = 'https://lucky-eclair-504d7b.netlify.app/'
+const webAppUrl = 'https://lucky-eclair-504d7b.netlify.app'
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
@@ -13,7 +13,7 @@ bot.on("message", async (msg) => {
     await bot.sendMessage(chatId, 'Ниже появится кнопка, нажми её', {
         reply_markup: {
             keyboard: [
-                [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
+                [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
             ]
         }
     });
@@ -24,5 +24,21 @@ bot.on("message", async (msg) => {
             ]
         }
     });
+  }
+
+  if(msg?.web_app_data?.data) {
+    try {
+        const data = JSON.parse(msg?.web_app_data?.data)
+        console.log(data);
+        await bot.sendMessage(chatId, 'Данные приняты, ждем подтверждение')
+        await bot.sendMessage(chatId, 'Ваша страна: ' + data.country)
+        await bot.sendMessage(chatId, 'Ваша улица: ' + data.street)
+
+        setTimeout( async () => {
+            await bot.sendMessage(chatId, 'Дальнейшую информацию вы получите в этом чате')
+        }, 2000)
+    } catch (error) {
+        console.log(error);
+    }
   }
 });
