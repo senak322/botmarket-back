d723020b1968a5ef9c9ea56d50dcb3a4399972c7
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-13 04:34:05 +0700

    add

--- index.js
+++ index.js
@@ -57,25 +57,22 @@ bot.on("message", async (msg) => {
   }
 });
 
-app.post("/web-data", async (req, res) => {
-  const { products = [], totalPrice, queryId } = req.body;
+app.post('/web-data', async (req, res) => {
+  const {queryId, products = [], totalPrice} = req.body;
   try {
-    await bot.answerWebAppQuery(queryId, {
-      type: "article",
-      id: queryId,
-      title: "Успешная покупка",
-      input_message_content: {
-        message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products
-          .map((item) => item.title)
-          .join(", ")}`,
-      },
-    });
-    return res.status(200).json({});
+      await bot.answerWebAppQuery(queryId, {
+          type: 'article',
+          id: queryId,
+          title: 'Успешная покупка',
+          input_message_content: {
+              message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+          }
+      })
+      return res.status(200).json({});
   } catch (e) {
-    console.log(e);
-    return res.status(500).json({});
+      return res.status(500).json({})
   }
-});
+})
 
 const PORT = 8000;
 app.listen(PORT, () => {
