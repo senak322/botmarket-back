f2cf931f705029e6eee108a70d41d2489cdc958f
Aleksandr Viznyi <senak98@yandex.ru>
2023-10-13 03:37:47 +0700

    some fixes

--- index.js
+++ index.js
@@ -58,22 +58,19 @@ bot.on("message", async (msg) => {
 });
 
 app.post("/web-data", async (req, res) => {
-  const { products, totalPrice, queryId } = req.body;
+  const { products = [], totalPrice, queryId } = req.body;
   try {
     await bot.answerWebAppQuery(queryId, {
-      type: 'article',
+      type: "article",
       id: queryId,
-      title: 'Успешная покупка',
-      input_message_content: {message_text: 'Поздравляю с покупкой, вы приобрели товар на сумму ' + totalPrice}
+      title: "Успешная покупка",
+      input_message_content: {
+        message_text:
+        ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+      },
     });
-    return res.status(200).json({})
+    return res.status(200).json({});
   } catch (e) {
-    await bot.answerWebAppQuery(queryId, {
-      type: 'article',
-      id: queryId,
-      title: 'Не удалось совершить покупку',
-      input_message_content: {message_text: 'Не удалось совершить покупку'}
-    });
     return res.status(500).json({})
   }
 });
